70b4f31263376260952629c4a1e0a5d1665a4317
viatomEcho <wujuan@viatomtech.com>
2023-09-18 09:24:21 +0800

    优化注释

--- lpble/src/main/java/com/viatom/lpble/data/entity/UserEntity.kt
+++ lpble/src/main/java/com/viatom/lpble/data/entity/UserEntity.kt
@@ -15,11 +15,12 @@ import kotlinx.android.parcel.Parcelize
 data class UserEntity(
     @PrimaryKey
     val userId: Long = 0,
-    val name: String,
-    val phone: String,
-    val gender: String,
-    val birthday: String,
-    val id_number: String,
-//    val height: String = "", // AI 不必须
-//    val weight: String = "", // AI 不必须
+    val name: String, // 姓名，必填
+    val phone: String,  // 手机号，必填
+    val gender: String, // 性别（1：男；2：女）
+    val birthday: String,  // 生日（yyyy-MM-dd）
+    val id_number: String,  // 身份证号码，对于签字报告必填(国药项目可不填)
+    // gender、birthday会影响分析结果，请填写真实数据
+//    val height: String = "", //  身高cm，非必填
+//    val weight: String = "", // 体重kg，非必填
 ) : Parcelable
